d8f5165249ca2007fe9efa98c9283c014ad68a3b
vidurbh <vidurbh@gmail.com>
2026-06-09 01:33:56 +0530

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ const cors = require('cors');
 const app = express();
 const supabase = require('./supabase');
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
